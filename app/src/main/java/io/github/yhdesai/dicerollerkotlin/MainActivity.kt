b0a41a7a4bec307478c007033b2b3c0dc4439f71
Yash <yhdesai99@ymail.com>
2019-10-21 21:04:31 +0530

    Initial commit

---
+++ app/src/main/java/io/github/yhdesai/dicerollerkotlin/MainActivity.kt
@@ -0,0 +1,41 @@
+package io.github.yhdesai.dicerollerkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var rollButton: Button
+    private lateinit var diceImage: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        rollButton = findViewById(R.id.roll_button)
+        diceImage = findViewById(R.id.dice_image)
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val randomInt = Random.nextInt(6) + 1
+
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+
+    }
+
+
+}
